d3e1a2e905d07c0b6bd5443914125da2c66afaa5
Kasjful Kurniawan <kasful.bpp@gmail.com>
2022-11-21 21:52:32 +0800

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+    </MantineProvider>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import { Button } from '@mantine/core';
+
+const Home: NextPage = () => {
+  return (
+    <Button>test</Button>
+  )
+}
+
+export default Home
