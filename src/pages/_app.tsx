21f06db853e65d20af59eaeeab8804116a06b375
Kasjful Kurniawan <kasful.bpp@gmail.com>
2022-11-22 15:25:49 +0800

    chore: installing prettier & eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,19 @@
-import { AppProps } from 'next/app';
-import { MantineProvider } from '@mantine/core';
-import { RecoilRoot } from 'recoil';
+import React from "react";
+import { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <head>
-        <title>Todo List App</title>
-      </head>
-      <RecoilRoot>
-        <MantineProvider withGlobalStyles withNormalizeCSS>
-            <Component {...pageProps} />
-        </MantineProvider>
-      </RecoilRoot>
-    </>
-  );
+	return (
+		<>
+			<head>
+				<title>Todo List App</title>
+			</head>
+			<RecoilRoot>
+				<MantineProvider withGlobalStyles withNormalizeCSS>
+					<Component {...pageProps} />
+				</MantineProvider>
+			</RecoilRoot>
+		</>
+	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,48 @@
-import type { NextPage } from 'next'
-import { useState } from 'react';
-import { createStyles, Paper, Text, Container, Divider, TextInput, Textarea, Button } from '@mantine/core';
+import React from "react";
+import type { NextPage } from "next";
+import { useState } from "react";
+import { Paper, Text, Container, Divider, TextInput, Textarea, Button } from "@mantine/core";
+import { atom, useSetRecoilState, useRecoilValue  } from "recoil";
+
+const todoListState = atom({
+	key: "todoList",
+	default: []
+});
 
 const Home: NextPage = () => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const handleButton = () => {
-    alert(`${title} ${description}`)
-  };
-  return (
-    <Container size="xs" px="xs" py="md">
-      <Paper radius="md" p="xl" withBorder>
-        <Text size="lg" weight={900} >
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
+	const handleButton = () => {
+		alert(`${title} ${description}`);
+	};
+	return (
+		<>
+			<Container size="xs" px="xs" py="md">
+				<Paper radius="md" p="xl" withBorder>
+					<Text size="lg" weight={900} >
           Todo List App
-        </Text>
-        <Divider />
-        <div>
-            <TextInput label="Title" placeholder="What will you do?" value={title} onChange={(event) => setTitle(event.target.value)} />
-            <Textarea 
-                 label="Description" 
-                 placeholder="Description of task." 
-                 onChange={(event) => setDescription(event.target.value)}
-                 value={description} 
-                 autosize
-                 minRows={2}
-                 maxRows={4}/>
-            <Button mt={10} onClick={handleButton}>Add</Button>
-        </div>
+					</Text>
+					<Divider />
+					<div>
+						<TextInput 
+							label="Title" 
+							placeholder="What will you do?" 
+							value={title}  
+							onChange={(event) => setTitle(event.target.value)} />
+						<Textarea 
+							label="Description" 
+							placeholder="Description of task." 
+							value={description} 
+							onChange={(event) => setDescription(event.target.value)}
+							autosize
+							minRows={2}
+							maxRows={4}/>
+						<Button mt={10} onClick={handleButton}>Add</Button>
+					</div>
+				</Paper>
+			</Container>
+		</>
+	);
+};
 
-      </Paper>
-    </Container>
-  )
-}
-
-export default Home
+export default Home;
