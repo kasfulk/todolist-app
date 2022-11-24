f1205161c84fdd9e59d54ddeb1c89148b1d91ecc
Kasjful Kurniawan <kasful.bpp@gmail.com>
2022-11-24 11:56:53 +0800

    feat: installing PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa');
 
-module.exports = nextConfig;
+module.exports = withPWA({
+  dest: 'public',
+});
